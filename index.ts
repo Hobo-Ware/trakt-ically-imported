ed9d4430ed85f02a29707f70bf25cc882f8c35fb
Vlad Jerca <vlad.jerca@gmail.com>
2025-11-12 12:19:13 +0100

    style: reformat code

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import { LIBERATOR_EXPORT_PATH } from './src/env/LIBERATOR_EXPORT_PATH';
-import { rateLimit, client as trakt } from './src/trakt/client';
-import type { Movie, Show } from './src/types';
-import { readFile } from 'fs/promises';
+import { readFile } from "fs/promises";
+import { LIBERATOR_EXPORT_PATH } from "./src/env/LIBERATOR_EXPORT_PATH";
+import { rateLimit, client as trakt } from "./src/trakt/client";
+import type { Movie, Show } from "./src/types";
 
 async function traktUnlimited() {
     await rateLimit();
@@ -12,72 +12,67 @@ async function traktUnlimited() {
  * * * * *  Read liberator data  * * * * *
  */
 const shows: Show[] = JSON
-    .parse(await readFile(`${LIBERATOR_EXPORT_PATH}/shows.json`, 'utf-8'));
+    .parse(await readFile(`${LIBERATOR_EXPORT_PATH}/shows.json`, "utf-8"));
 
 const movies: Movie[] = JSON
-    .parse(await readFile(`${LIBERATOR_EXPORT_PATH}/movies.json`, 'utf-8'));
+    .parse(await readFile(`${LIBERATOR_EXPORT_PATH}/movies.json`, "utf-8"));
 
 /**
  * * * * *  Add shows and movies to watchlist  * * * * *
  */
 const showWatchlist = shows
-    .map(serie => ({
+    .map((serie) => ({
         ids: {
             tvdb: serie.id.tvdb,
-            imdb: serie.id.imdb === '-1'
-                ? ''
-                : serie.id.imdb,
-            slug: '',
+            imdb: serie.id.imdb === "-1" ? "" : serie.id.imdb,
+            slug: "",
             tmdb: -Infinity,
             trakt: -Infinity,
         },
     }));
 
 const movieWatchlist = movies
-    .map(movie => ({
+    .map((movie) => ({
         watched_at: movie.watched_at,
         ids: {
             imdb: movie.id.imdb,
-            slug: '',
+            slug: "",
             tmdb: -Infinity,
             trakt: -Infinity,
         },
     }));
 
 await traktUnlimited()
-    .then(trakt =>
+    .then((trakt) =>
         trakt.sync.watchlist.add({
             shows: showWatchlist,
             movies: movieWatchlist,
-
-        }),
+        })
     )
-    .then(async response => {
-        console.log('--- Imported watchlist: ', await response.text());
+    .then(async (response) => {
+        console.log("--- Imported watchlist: ", await response.text());
     });
 
 /**
  * * * * *  Add show and movie watches to history  * * * * *
  */
 const showHistory = shows
-    .map(show => ({
+    .map((show) => ({
         ids: {
             tvdb: show.id.tvdb,
-            imdb: show.id.imdb === '-1'
-                ? ''
-                : show.id.imdb,
-            slug: '',
+            imdb: show.id.imdb === "-1" ? "" : show.id.imdb,
+            slug: "",
             tmdb: -Infinity,
             trakt: -Infinity,
         },
         seasons: show
             .seasons
-            .map(season => ({
+            .map((season) => ({
                 number: season.number,
                 episodes: season
                     .episodes
-                    .filter(episode => episode.is_watched)
-                    .map(episode => ({
+                    .filter((episode) => episode.is_watched)
+                    .map((episode) => ({
                         watched_at: episode.watched_at,
                         number: episode.number,
                     })),
@@ -85,69 +80,73 @@ const showHistory = shows
     }));
 
 const movieHistory = movieWatchlist
-    .filter(movie => movie.watched_at != null);
+    .filter((movie) => movie.watched_at != null);
 
 await traktUnlimited()
-    .then(trakt =>
+    .then((trakt) =>
         trakt.sync.history.add({
             /**
              * Library typings are not compliant with Trakt Apiary documentation.
-             * 
+             *
              * See: https://trakt.docs.apiary.io/#reference/sync/add-to-history/add-items-to-watched-history
              */
             // @ts-expect-error
             shows: showHistory,
             movies: movieHistory,
-        }),
+        })
     )
-    .then(async response => {
-        console.log('--- Imported watch history: ', await response.text());
+    .then(async (response) => {
+        console.log("--- Imported watch history: ", await response.text());
     });
 
 /***
  * * * * *  Add stopped shows to hidden sections  * * * * *
  */
 const stoppedShows = shows
-    .filter(show => show.status === 'stopped')
-    .map(show => ({
+    .filter((show) => show.status === "stopped")
+    .map((show) => ({
         ids: {
             tvdb: show.id.tvdb,
-            imdb: show.id.imdb === '-1'
-                ? ''
-                : show.id.imdb,
-            slug: '',
+            imdb: show.id.imdb === "-1" ? "" : show.id.imdb,
+            slug: "",
             tmdb: -Infinity,
             trakt: -Infinity,
-        }
+        },
     }));
 
 await traktUnlimited()
-    .then(trakt =>
+    .then((trakt) =>
         trakt
             .users
             .requests
             .hidden
             .add({
                 shows: stoppedShows,
-                section: 'progress_watched',
+                section: "progress_watched",
             })
-            .then(async response => {
-                console.log('--- Stopped shows hiiden from progress: ', await response.text());
+            .then(async (response) => {
+                console.log(
+                    "--- Stopped shows hiiden from progress: ",
+                    await response.text(),
+                );
             })
     );
 
 await traktUnlimited()
-    .then(trakt =>
+    .then((trakt) =>
         trakt
             .users
             .requests
             .hidden
             .add({
                 shows: stoppedShows,
-                section: 'recommendations',
+                section: "recommendations",
             })
-            .then(async response => {
-                console.log('--- Stopped shows hidden from recommendations: ', await response.text());
+            .then(async (response) => {
+                console.log(
+                    "--- Stopped shows hidden from recommendations: ",
+                    await response.text(),
+                );
             })
     );
 
